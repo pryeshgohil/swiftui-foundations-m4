c86779a34ff408af99da210895e7f34c31a5c387
Pryesh Gohil <pryeshgohil@hotmail.com>
2022-12-11 21:27:05 +0000

    Initial Commit

---
+++ recipe-list-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  recipe-list-app
+//
+//  Created by Pryesh Gohil on 11/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
